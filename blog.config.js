a410e7a33f0ed17c59d0d08cd68535648e038740
AndrewTs'ui <38204629+tsuienpeng@users.noreply.github.com>
2024-05-11 16:13:19 +0800

    Update blog.config.js
    开启主题切换
    关闭鼠标特效

--- blog.config.js
+++ blog.config.js
@@ -245,8 +245,10 @@ const BLOG = {
     '251, 243, 140'
   ],
 
+  THEME_SWITCH: process.env.NEXT_PUBLIC_THEME_SWITCH || true, // 是否显示切换主题按钮
+
   // 鼠标跟随特效
-  MOUSE_FOLLOW: process.env.NEXT_PUBLIC_MOUSE_FOLLOW || true, // 开关
+  MOUSE_FOLLOW: process.env.NEXT_PUBLIC_MOUSE_FOLLOW || false, // 开关
   // 这两个只有在鼠标跟随特效开启时才生效
   // 鼠标类型 1：路劲散点 2：下降散点 3：上升散点 4：边缘向鼠标移动散点 5：跟踪转圈散点 6：路径线条 7：聚集散点 8：聚集网格 9：移动网格 10：上升粒子 11：转圈随机颜色粒子 12：圆锥放射跟随蓝色粒子
   MOUSE_FOLLOW_EFFECT_TYPE: 11, // 1-12
